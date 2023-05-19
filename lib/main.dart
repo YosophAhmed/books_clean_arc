7b722603abff3ba283b3f08f434d1eb6b20d921f
Yosoph <devyosoph@gmail.com>
2023-05-19 12:19:46 +0300

    Adding Hive Package

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,9 @@ class Bookly extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        textTheme: GoogleFonts.montserratTextTheme(
+          ThemeData.dark().textTheme,
+        ),
       ),
     );
   }
