3e3184f3de4b9f1abb4ec9b837200bd9a4a4a6ff
Yosoph <devyosoph@gmail.com>
2023-05-24 19:19:33 +0300

    Failure Class done

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
@@ -27,8 +28,17 @@ class HomeRepoImpl extends HomeRepo {
         books,
       );
     } catch (error) {
+      if (error is DioError) {
+        return Left(
+          ServerFailure.fromDioError(
+            error: error,
+          ),
+        );
+      }
       return Left(
-        Failure(),
+        ServerFailure(
+          failureMessage: error.toString(),
+        ),
       );
     }
   }
@@ -46,8 +56,17 @@ class HomeRepoImpl extends HomeRepo {
         books,
       );
     } catch (error) {
+      if (error is DioError) {
+        return Left(
+          ServerFailure.fromDioError(
+            error: error,
+          ),
+        );
+      }
       return Left(
-        Failure(),
+        ServerFailure(
+          failureMessage: error.toString(),
+        ),
       );
     }
   }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,3 +1,82 @@
-class Failure {
+import 'package:dio/dio.dart';
 
+abstract class Failure {
+  final String failureMessage;
+
+  Failure({
+    required this.failureMessage,
+  });
 }
+
+class ServerFailure extends Failure {
+  ServerFailure({
+    required super.failureMessage,
+  });
+
+  factory ServerFailure.fromDioError({
+    required DioError error,
+  }) {
+    switch (error.type) {
+      case DioErrorType.connectionTimeout:
+        return ServerFailure(
+          failureMessage: 'Connection time out',
+        );
+      case DioErrorType.sendTimeout:
+        return ServerFailure(
+          failureMessage: 'Send time out',
+        );
+      case DioErrorType.receiveTimeout:
+        return ServerFailure(
+          failureMessage: 'Receive time out',
+        );
+      case DioErrorType.badCertificate:
+        return ServerFailure(
+          failureMessage: 'Bad certificate',
+        );
+      case DioErrorType.badResponse:
+        return ServerFailure.fromResponse(
+          statusCode: error.response?.statusCode ?? 0,
+          response: error.response!.data,
+        );
+      case DioErrorType.cancel:
+        return ServerFailure(
+          failureMessage: 'canceled',
+        );
+      case DioErrorType.connectionError:
+        return ServerFailure(
+          failureMessage: 'No Internet connection',
+        );
+      case DioErrorType.unknown:
+        return ServerFailure(
+          failureMessage: 'Unknown error',
+        );
+    }
+  }
+
+  factory ServerFailure.fromResponse({
+    required int statusCode,
+    required dynamic response,
+  }) {
+    if (statusCode == 404) {
+      return ServerFailure(
+        failureMessage: 'Your request not found',
+      );
+    } else if (statusCode == 500) {
+      return ServerFailure(
+        failureMessage: 'Server Failure',
+      );
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(
+        failureMessage: response['error']['message'],
+      );
+    } else {
+      return ServerFailure(
+        failureMessage: 'Unknown error',
+      );
+    }
+  }
+}
+//
+// class CacheFailure extends Failure {}
+//
+// class NetworkFailure extends Failure {}
